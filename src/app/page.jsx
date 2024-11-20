909580de75f5d3d086b66b859ad9ebb40c96d760
Ayushi Singh <ayushisingh952891@gmail.com>
2024-11-20 18:57:24 +0530

    commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+
 import ParticlesComponent from "../components/particles";
 import Navbar from "../sections/Navbar";
 import Hero from "../sections/Hero";
@@ -6,7 +6,7 @@ import About from '../sections/About'
 import Projects from '../sections/Projects'
 import Experience from '../sections/Experience'
 import Contact from '../sections/Contact'
-import Footer from '../sections/Footer'
+
 export default function Home() {
   return (
     <div>
--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,6 +1,6 @@
 "use client"
 import { useState } from 'react';
-import Globe from 'react-globe.gl';
+
 
 import Button from '../components/Button.jsx';
 
